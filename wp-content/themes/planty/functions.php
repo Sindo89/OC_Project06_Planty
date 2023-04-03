87d03b205333cf1004deb9d2dc4efc4e4658d3e3
Darini <darini.kevin@gmail.com>
2023-04-03 18:32:06 +0200

    Add theme.css and header files

--- wp-content/themes/planty/functions.php
+++ wp-content/themes/planty/functions.php
@@ -4,12 +4,12 @@ add_action('wp_enqueue_scripts', 'my_theme_enqueue_styles');
 function my_theme_enqueue_styles()
 {
 
-  $parent_style = 'parent-style'; // This is 'planty-style' for the Planty theme.
+  $parent_style = 'parent-style';
 
   wp_enqueue_style($parent_style, get_template_directory_uri() . '/style.css');
   wp_enqueue_style(
     'child-style',
-    get_stylesheet_directory_uri() . '/style.css',
+    get_stylesheet_directory_uri() . '/assets/css/theme.css',
     array($parent_style),
     wp_get_theme()->get('Version')
   );
