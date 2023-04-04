347080e6a89e5e2b091e1cea49d2d7d263e996be
Darini <darini.kevin@gmail.com>
2023-04-04 14:40:43 +0200

    Add footer

---
+++ wp-content/themes/planty/footer.php
@@ -0,0 +1,12 @@
+</main>
+</div>
+<footer id="footer" role="contentinfo">
+  <div id="copyright">
+    <p>Mentions légales</p>
+  </div>
+</footer>
+</div>
+<?php wp_footer(); ?>
+</body>
+
+</html>
