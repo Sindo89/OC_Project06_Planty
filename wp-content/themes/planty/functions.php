a3787b990957e7313a0c52e52f8a6cf3ad7fc7d9
Darini <darini.kevin@gmail.com>
2023-04-05 18:50:23 +0200

    Add hook for admin menu, css for hover and form

--- wp-content/themes/planty/functions.php
+++ wp-content/themes/planty/functions.php
@@ -4,13 +4,20 @@ add_action('wp_enqueue_scripts', 'my_theme_enqueue_styles');
 function my_theme_enqueue_styles()
 {
 
-  $parent_style = 'parent-style';
-
-  wp_enqueue_style($parent_style, get_template_directory_uri() . '/style.css');
+  wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
   wp_enqueue_style(
     'child-style',
     get_stylesheet_directory_uri() . '/assets/css/theme.css',
-    array($parent_style),
+    array('parent-style'),
     wp_get_theme()->get('Version')
   );
 }
+
+function add_admin_link_to_menu($items, $args)
+{
+  if (is_user_logged_in() && has_nav_menu('main-menu')) { // vérifie que l'utilisateur est connecté et que le menu existe
+    $items = '<li><a href="/nous-rencontrer/">Nous rencontrer</a></li> <li><a href="' . admin_url() . '">Admin</a></li> <li><a href="/commander/)">Commander</a></li>'; // ajoute le lien vers l'admin
+  }
+  return $items; // retourne le menu
+}
+add_filter('wp_nav_menu_items', 'add_admin_link_to_menu', 10, 2); // ajoute le filtre pour ajouter le lien vers l'admin dans le menu
