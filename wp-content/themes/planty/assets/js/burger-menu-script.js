724b5a3c2de4f26c9f161f16461a43f808e338ee
Darini <darini.kevin@gmail.com>
2023-04-11 16:41:22 +0200

    Add comments to code

--- wp-content/themes/planty/assets/js/burger-menu-script.js
+++ wp-content/themes/planty/assets/js/burger-menu-script.js
@@ -1,10 +1,11 @@
 document.addEventListener("DOMContentLoaded", function () {
-  let toggleburger = document.querySelector(".toggleburger");
-  let header = document.querySelector("header");
+  // attend que le DOM soit chargé (tous les éléments HTML)
+  let toggleburger = document.querySelector(".toggleburger"); // sélectionne l'élément avec la classe toggleburger
+  let header = document.querySelector("header"); // sélectionne l'élément header
 
   toggleburger.addEventListener("click", function () {
-    toggleburger.classList.toggle("active");
-    header.classList.toggle("open");
-    jQuery(".menu ul").slideToggle(500);
+    // écoute le click sur l'élément toggleburger
+    toggleburger.classList.toggle("active"); // ajoute ou supprime la classe active
+    header.classList.toggle("open"); // ajoute ou supprime la classe open
   });
 });
--- wp-content/themes/planty/functions.php
+++ wp-content/themes/planty/functions.php
@@ -1,9 +1,12 @@
 <?php
 
+
+//******************************************************************
+//Ajoute le style parent (style.css) et le style enfant (theme.css)
+
 add_action('wp_enqueue_scripts', 'my_theme_enqueue_styles');
 function my_theme_enqueue_styles()
 {
-
   wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
   wp_enqueue_style(
     'child-style',
@@ -13,23 +16,48 @@ function my_theme_enqueue_styles()
   );
 }
 
+//******************************************************************
+
+
+
+
+
+//******************************************************************
+//Ajoute le lien vers l'admin dans le menu
+
 function add_admin_link_to_menu($items, $args)
 {
-  if (is_user_logged_in() && has_nav_menu('main-menu')) { // vérifie que l'utilisateur est connecté et que le menu existe
-    $items .= '<li class="admin-menu"><a href="' . admin_url() . '">Admin</a></li>'; // ajoute le lien vers l'admin
+  if (is_user_logged_in() && has_nav_menu('main-menu')) {
+    $items .= '<li class="admin-menu"><a href="' . admin_url() . '">Admin</a></li>';
   }
-  return $items; // retourne le menu
+  return $items;
 }
-add_filter('wp_nav_menu_items', 'add_admin_link_to_menu', 10, 2); // ajoute le filtre pour ajouter le lien vers l'admin dans le menu
+add_filter('wp_nav_menu_items', 'add_admin_link_to_menu', 10, 2);
+
+//******************************************************************
 
 
-add_filter('wpcf7_form_elements', 'delicious_wpcf7_form_elements');
+
+
+
+//******************************************************************
+//Permet l'utilisation de shortcode dans wpcf7
 
 function delicious_wpcf7_form_elements($form)
 {
   $form = do_shortcode($form);
   return $form;
 }
+add_filter('wpcf7_form_elements', 'delicious_wpcf7_form_elements');
+
+//******************************************************************
+
+
+
+
+
+//******************************************************************
+//Execute le script burger-menu-script.js
 
 function wpmu_burger_menu_scripts()
 {
@@ -37,3 +65,5 @@ function wpmu_burger_menu_scripts()
   wp_enqueue_script('burger-menu-script', get_stylesheet_directory_uri() . '/assets/js/burger-menu-script.js', array('jquery'));
 }
 add_action('wp_enqueue_scripts', 'wpmu_burger_menu_scripts');
+
+//******************************************************************
